6f77e099cb833c7b24077f6dad25aec0fa5823ef
annumd <161940207+annumd@users.noreply.github.com>
2024-10-14 16:35:42 +0530

    Update sparse.c

--- sparse.c
+++ sparse.c
@@ -6,7 +6,8 @@ void add(int a[][max], int aSize, int b[][max], int bSize, int sum[][max], int *
 void display(int a[][max], int size);
 void transpose(int a[][max], int transposed[][max], int row, int col, int *transposedSize);
 
-int main() {
+int main() 
+{
     int mat[max][max], s[max][max], sum[max][max];
     int i, j, row, col, con, ch, sr, sumSize, transposedSize;
 
@@ -14,15 +15,19 @@ int main() {
     scanf("%d%d", &row, &col);
 
     printf("Enter the numbers:\n");
-    for (i = 0; i < row; i++) {
-        for (j = 0; j < col; j++) {
+    for (i = 0; i < row; i++) 
+    {
+        for (j = 0; j < col; j++) 
+        {
             scanf("%d", &mat[i][j]);
         }
     }
 
     printf("The matrix is:\n");
-    for (i = 0; i < row; i++) {
-        for (j = 0; j < col; j++) {
+    for (i = 0; i < row; i++) 
+    {
+        for (j = 0; j < col; j++) 
+        {
             printf("%d\t", mat[i][j]);
         }
         printf("\n");
@@ -37,21 +42,26 @@ int main() {
         scanf("%d", &ch);
 
         switch (ch) {
-            case 1: {
+            case 1: 
+                {
                 int b[max][max], sec[max][max], l;
                 printf("Enter the number of rows and columns of the second matrix: ");
                 scanf("%d%d", &row, &col);
 
                 printf("Enter the numbers:\n");
-                for (i = 0; i < row; i++) {
-                    for (j = 0; j < col; j++) {
+                for (i = 0; i < row; i++) 
+                {
+                    for (j = 0; j < col; j++) 
+                    {
                         scanf("%d", &b[i][j]);
                     }
                 }
 
                 printf("The matrix is:\n");
-                for (i = 0; i < row; i++) {
-                    for (j = 0; j < col; j++) {
+                for (i = 0; i < row; i++) 
+                {
+                    for (j = 0; j < col; j++) 
+                    {
                         printf("%d\t", b[i][j]);
                     }
                     printf("\n");
@@ -61,19 +71,23 @@ int main() {
                 printf("\n-Sparse Matrix Representation-\n");
                 display(sec, l);
 
-                if (s[0][0] == sec[0][0] && s[0][1] == sec[0][1]) {
+                if (s[0][0] == sec[0][0] && s[0][1] == sec[0][1])
+                {
                     sumSize = 0;
                     int rows = s[0][0], cols = s[0][1];
                     add(s, sr, sec, l, sum, &sumSize, rows, cols);
                     printf("\nSum of Sparse Matrices:\n");
                     display(sum, sumSize);
-                } else {
+                }
+                else 
+                {
                     printf("Matrix dimensions do not match for addition.\n");
                 }
             }
             break;
 
-            case 2: {
+            case 2:
+                {
                 int transposed[max][max];
                 transpose(mat, transposed, row, col, &transposedSize);
                 printf("\nTranspose Sparse Matrix Representation:\n");
@@ -112,10 +126,13 @@ int sparse(int a[][max], int b[][max], int row, int col) {
     return k - 1;
 }
 
-void display(int a[][max], int size) {
+void display(int a[][max], int size)
+{
     int i, j;
-    for (i = 0; i <= size; i++) {
-        for (j = 0; j < 3; j++) {
+    for (i = 0; i <= size; i++)
+        {
+        for (j = 0; j < 3; j++) 
+        {
             printf("%d\t", a[i][j]);
         }
         printf("\n");
@@ -126,7 +143,8 @@ void add(int a[][max], int aSize, int b[][max], int bSize, int sum[][max], int *
     int i = 1, j = 1, k = 1;
 
     while (i <= aSize && j <= bSize) {
-        if (a[i][0] == b[j][0] && a[i][1] == b[j][1]) {
+        if (a[i][0] == b[j][0] && a[i][1] == b[j][1]) 
+        {
             sum[k][0] = a[i][0];
             sum[k][1] = a[i][1];
             sum[k][2] = a[i][2] + b[j][2];
